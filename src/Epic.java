386c7247358d4803ad4de09baf6517f57e472049
Natali-23 <shtegman.nata@yandex.ru>
2025-09-02 20:53:56 +0300

    Epic.java

--- src/Epic.java
+++ src/Epic.java
@@ -2,48 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Epic extends Task {
-    private List<Integer> subtaskIds;
-
-    public Epic(String name, String description) {
-        super(name, description);
-        this.subtaskIds = new ArrayList<>();
-    }
+    private final List<Integer> subtaskIds = new ArrayList<>();
 
     public List<Integer> getSubtaskIds() {
         return subtaskIds;
     }
 
-    public void addSubtaskId(int id) {
-        subtaskIds.add(id);
-    }
-
-    public void removeSubtaskId(int id) {
-        subtaskIds.remove((Integer) id);
-    }
-
-    public void clearSubtaskIds() {
-        subtaskIds.clear();
-    }
-
-    @Override
-    public void setStatus(Status status) {
-        throw new UnsupportedOperationException(
-                "Статус эпика рассчитывается автоматически."
-        );
-    }
-
-
-    protected void updateStatus(Status status) {
-        super.setStatus(status);
-    }
-
     @Override
     public String toString() {
         return "Epic{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", status=" + status +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", status=" + getStatus() +
                 ", subtaskIds=" + subtaskIds +
                 '}';
     }
