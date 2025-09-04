78018ae89b36672d55ba4ed0e61b2a902c9b23c4
Natali-23 <shtegman.nata@yandex.ru>
2025-09-04 11:02:43 +0300

    Subtask.java

--- src/Subtask.java
+++ src/Subtask.java
@@ -1,22 +1,14 @@
+package task;
+
 public class Subtask extends Task {
-    private int epicId;
+    private final int epicId;
 
-    public int getEpicId() {
-        return epicId;
-    }
-
-    public void setEpicId(int epicId) {
+    public Subtask(String name, String description, int epicId) {
+        super(name, description);
         this.epicId = epicId;
     }
 
-    @Override
-    public String toString() {
-        return "Subtask{" +
-                "id=" + getId() +
-                ", name='" + getName() + '\'' +
-                ", description='" + getDescription() + '\'' +
-                ", status=" + getStatus() +
-                ", epicId=" + epicId +
-                '}';
+    public int getEpicId() {
+        return epicId;
     }
 }
