655eca869bf32c25cc1b7d65473f489c97587ffb
Natali-23 <shtegman.nata@yandex.ru>
2025-09-04 12:50:09 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 package taskmanager;
 
-import taskmanager.manager.TaskManager;
-import taskmanager.model.*;
-
-
 public class Main {
     public static void main(String[] args) {
         TaskManager manager = new TaskManager();
